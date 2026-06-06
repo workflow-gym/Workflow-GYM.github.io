0c538ed50a8b589231846505aee2402e887c2855
Liu Yiwei <lew1sin@163.com>
2026-06-07 00:33:50 +0800

    6th Jul, 2026

--- static/js/index.js
+++ static/js/index.js
@@ -83,6 +83,7 @@ function scrollToTop() {
 // Show/hide scroll to top button
 window.addEventListener('scroll', function() {
     const scrollButton = document.querySelector('.scroll-to-top');
+    if (!scrollButton) return;
     if (window.pageYOffset > 300) {
         scrollButton.classList.add('visible');
     } else {
